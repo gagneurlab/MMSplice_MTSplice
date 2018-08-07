add13f74a8e4f84589c6c69ce990decfb7bdde32
Jun <chengju@in.tum.de>
2018-08-07 15:19:59 +0200

    modify postproc

--- eis/utils/postproc.py
+++ eis/utils/postproc.py
@@ -8,9 +8,11 @@ def max_varEff(df, combine_fn=lambda x: np.sum(x, axis=1)):
 		combine_fn: Maximum effect size is calculated by calculating 
 		the combined effect size of all 5 modules.
 	"""
+	if isinstance(df, str):
+		df = pd.read_csv(df, index_col=0)
 	ref_list = ['EIS_ref_acceptorIntron', 'EIS_ref_acceptor', 'EIS_ref_exon', 'EIS_ref_donor', 'EIS_ref_donorIntron']
 	alt_list = ['EIS_alt_acceptorIntron', 'EIS_alt_acceptor', 'EIS_alt_exon', 'EIS_alt_donor', 'EIS_alt_donorIntron']
-	EIS_diff = df[alt_list].as_matrix() - df[ref_list].as_matrix()
+	EIS_diff = df[alt_list].values - df[ref_list].values
 	df['EIS_diff'] = combine_fn(EIS_diff)
 	dfMax = df.groupby(['ID'], as_index=False).agg({'EIS_diff': lambda x: max(x, key=abs)})
 	dfMax = dfMax.merge(df, how='left', on = ['ID', 'EIS_diff'])
