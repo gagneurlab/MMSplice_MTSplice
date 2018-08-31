b043de675aaef75e47d3071405d3706815fcc1df
Jun <chengju@in.tum.de>
2018-08-31 11:12:27 +0200

    bump

--- eis/__init__.py
+++ eis/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Jun Cheng"""
 __email__ = 'chengju@in.tum.de'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from eis.eis import Eis, predict_all, writeEIS, predict_all_table
 import eis.generic
