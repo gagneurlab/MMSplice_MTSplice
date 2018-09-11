dbbbcb6cd6f7e410d7e98b2ab0490aa22661b509
Jun <chengju@in.tum.de>
2018-09-11 11:21:33 +0200

    update example

--- eis/__init__.py
+++ eis/__init__.py
@@ -12,3 +12,13 @@ import eis.IntervalTree
 import eis.vcf_dataloader
 import eis.exon_dataloader
 from .utils import postproc
+
+# export modules
+from eis.eis import ACCEPTOR_INTRON, ACCEPTOR, EXON, DONOR, DONOR_INTRON 
+from keras.models import load_model
+
+ACCEPTOR_INTRON_MODULE = load_model(ACCEPTOR_INTRON)
+ACCEPTOR_MODULE = load_model(ACCEPTOR)
+EXON_MODULE = load_model(EXON)
+DONOR_MODULE = load_model(DONOR)
+DONOR_INTRON_MODULE = load_model(DONOR_INTRON)
