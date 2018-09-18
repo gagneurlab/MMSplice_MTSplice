17a3d668536bc2b59016ad5b053021fc20f537a5
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2018-09-18 14:06:54 +0300

    EIS renamed to MMSplice and #3 fixed

--- perl/MMSplice.pm
+++ perl/MMSplice.pm
@@ -1,6 +1,6 @@
 =head1 LICENSE
 
-Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [1999-2015] Wellcome EMBL-European Bioinformatics Institute
 Copyright [2016-2018] EMBL-European Bioinformatics Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,38 +23,25 @@ limitations under the License.
 
 =head1 NAME
 
- Eis
+ MMSplice
 
 =head1 SYNOPSIS
 
- mv Eis.pm ~/.vep/Plugins
- pip install eis
- ./vep -i variants.vcf --plugin Eis,[intronl_len=100],[intronr_len=80],[exon_cut_l=0],[exon_cut_r=0],[acceptor_intron_cut=6],[donor_intron_cut=3],[acceptor_intron_len=20],[acceptor_exon_len=3],[donor_exon_len=3],[donor_intron_len=6],[port_of_eis_server]
+ mv MMSplice.pm ~/.vep/Plugins
+ pip install mmsplice
+ ./vep -i variants.vcf --plugin MMSplice,[port_of_mmsplice_server=5000],[intronl_len=100],[intronr_len=80],[exon_cut_l=0],[exon_cut_r=0],[acceptor_intron_cut=6],[donor_intron_cut=3],[acceptor_intron_len=20],[acceptor_exon_len=3],[donor_exon_len=3],[donor_intron_len=6],[acceptor_intronM],[acceptorModelFile],[exonModelFile],[donorModelFile],[donor_intronModelFile]
 
 
 =head1 DESCRIPTION
 
  This is a plugin for the Ensembl Variant Effect Predictor (VEP) that
- runs MaxEntScan (http://genes.mit.edu/burgelab/maxent/Xmaxentscan_scoreseq.html)
- to get splice site predictions.
+ runs MMSplice.
 
- The plugin copies most of the code verbatim from the score5.pl and score3.pl
- scripts provided in the MaxEntScan download. To run the plugin you must get and
- unpack the archive from http://genes.mit.edu/burgelab/maxent/download/; the path
- to this unpacked directory is then the param you pass to the --plugin flag.
-
- The plugin executes the logic from one of the scripts depending on which
- splice region the variant overlaps:
-
- score5.pl : last 3 bases of exon    --> first 6 bases of intron
- score3.pl : last 20 bases of intron --> first 3 bases of exon
-
- The plugin reports the reference, alternate and difference (REF - ALT) maximum
- entropy scores.
+Add more doc.
 
 =cut
 
-package Eis;
+package MMSplice;
 
 use strict;
 use warnings;
@@ -70,7 +57,6 @@ use Bio::EnsEMBL::Variation::Utils::VariationEffect qw(overlap);
 use Bio::EnsEMBL::Variation::Utils::BaseVepPlugin;
 use base qw(Bio::EnsEMBL::Variation::Utils::BaseVepPlugin);
 
-# how many seq/score pairs to cache in memory
 our $CACHE_SIZE = 50;
 
 sub new {
@@ -80,27 +66,25 @@ sub new {
   $self->init_params();
   $self->init_api();
 
-
   return $self;
 }
 
 sub feature_types {
   return ['Transcript'];
-  # return ['intergenic_variant'];
 }
 
 sub get_header_info {
     return {
-      acceptor_intron_ref => "acceptor intron ref",
-      acceptor_ref => "acceptor ref",
-      exon_ref => "exon ref",
-      donor_ref => "donor ref",
-      donor_intron_ref => "donor intron ref",
-      acceptor_intron_alt => "acceptor intron alt",
-      acceptor_alt => "acceptor alt",
-      exon_alt => "exon alt",
-      donor_alt => "alt donor",
-      donor_intron_alt => "alt donor intron"
+      ref_acceptor_intron => "acceptor intron ref",
+      ref_acceptor => "acceptor ref",
+      ref_exon => "exon ref",
+      ref_donor => "donor ref",
+      ref_donor_intron => "donor intron ref",
+      alt_acceptor_intron => "acceptor intron alt",
+      alt_acceptor => "acceptor alt",
+      alt_exon => "exon alt",
+      alt_donor => "alt donor",
+      alt_donor_intron => "alt donor intron"
     };
 }
 
@@ -108,15 +92,10 @@ sub init_params {
   my $self = shift;
   my $params = $self->params;
 
-  $self->{acceptor_intronM} = shift @$params || "";
-  $self->{acceptorM} = shift @$params || "";
-  $self->{exonM} = shift @$params || "";
-  $self->{donorM} = shift @$params || "";
-  $self->{donor_intronM} = shift @$params || "";
   $self->{api_port} = shift @$params || 5000;
 
   $self->{overhang_l} = shift @$params || 100;
-  $self->{overhang_r} = shift @$params || 100;
+  $self->{overhang_r} = shift @$params || 80;
   $self->{exon_cut_l} = shift @$params || 0;
   $self->{exon_cut_r} = shift @$params || 0;
   $self->{acceptor_intron_cut} = shift @$params || 6;
@@ -125,6 +104,12 @@ sub init_params {
   $self->{acceptor_exon_len} = shift @$params || 3;
   $self->{donor_exon_len} = shift @$params || 5;
   $self->{donor_intron_len} = shift @$params || 13;
+
+  $self->{acceptor_intronM} = shift @$params || "";
+  $self->{acceptorM} = shift @$params || "";
+  $self->{exonM} = shift @$params || "";
+  $self->{donorM} = shift @$params || "";
+  $self->{donor_intronM} = shift @$params || "";
 }
 
 sub init_api {
@@ -140,22 +125,16 @@ sub init_api {
 sub start_api {
   my $self = shift;
 
-  pipe(PARENTRECEIVE, CHILDSEND);
-
   my $pid = fork();
   die if not defined $pid;
-  if (not $pid) {
-      my $cid = $$;
-      print CHILDSEND "$cid\n";
-      `eis run_api`;
+
+  if ($pid == 0) {
+      exec("mmsplice run_api");
   }
-
-  my $child_id = <PARENTRECEIVE>;
-  chomp $child_id;
-  $self->{api_pid} = $child_id;
-
-  sleep(10); # find better solution.
-  print('api started');
+  else {
+      $self->{api_pid} = $pid;
+  }
+  sleep(10);
 }
 
 sub create_model {
@@ -211,16 +190,16 @@ sub run {
     my @alt_scores = $self->req_psi_score($alt_seq);
 
     return {
-      acceptor_intron_ref => $ref_scores[0],
-      acceptor_ref => $ref_scores[1],
-      exon_ref => $ref_scores[2],
-      donor_ref => $ref_scores[3],
-      donor_intron_ref => $ref_scores[4],
-      acceptor_intron_alt => $alt_scores[0],
-      acceptor_alt => $alt_scores[1],
-      exon_alt => $alt_scores[2],
-      donor_alt => $alt_scores[3],
-      donor_intron_alt => $alt_scores[4]
+      ref_acceptor_intron => $ref_scores[0],
+      ref_acceptor => $ref_scores[1],
+      ref_exon => $ref_scores[2],
+      ref_donor => $ref_scores[3],
+      ref_donor_intron => $ref_scores[4],
+      alt_acceptor_intron => $alt_scores[0],
+      alt_acceptor => $alt_scores[1],
+      alt_exon => $alt_scores[2],
+      alt_donor => $alt_scores[3],
+      alt_donor_intron => $alt_scores[4]
     }
   }
 
@@ -274,8 +253,8 @@ sub overlap_exons {
       my ($low, $high) = ($exon->{start}, $exon->{end});
       my ($start, $end) = ($vf_start - $self->{overhang_l}, $vf_end + $self->{overhang_r});
 
-      unless ($high < $start or $low > $end){
-	push @exons, $exon;
+      unless ($high < $start or $low > $end) {
+          push @exons, $exon;
       }
     }
 
@@ -382,7 +361,8 @@ sub fetch_seq {
 
 sub DESTROY {
     my $self = shift;
-    kill 1, $self->{api_pid};
+
+    kill 2, $self->{api_pid};
     print('api closed');
 }
 
