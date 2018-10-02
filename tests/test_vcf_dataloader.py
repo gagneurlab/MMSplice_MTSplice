fe1ea88f563129c6ee2e32fd827d39f4f4c9db79
muhammedhasan <muhammedhasancelik@gmail.com>
2018-10-02 20:28:49 +0300

    tests added for predict_all_table

--- tests/test_vcf_dataloader.py
+++ tests/test_vcf_dataloader.py
@@ -1,11 +1,8 @@
 from collections import namedtuple
-import tempfile
-
-import pytest
 from mmsplice.vcf_dataloader import SplicingVCFDataloader, FastaSeq
 
-gtf_file = 'tests/data/test.gtf'
-fasta_file = 'tests/data/hg19.nochr.chr17.fa'
+from conftest import gtf_file, fasta_file, snps, deletions, \
+    insertions, variants
 
 
 def test_FastSeq_getSeq():
@@ -19,55 +16,6 @@ def test_FastSeq_getSeq():
     iv = IV(chrom='17', start=41267742, end=41267752, strand='-')
     seq = fasta.getSeq(iv)
     assert seq == 'TATTTTGCAAG'
-
-
-snps = [
-    "17:41276033:C:['G']"
-]
-
-deletions = [
-    "13:32953886:GTT:['AA']",
-    "17:41267742:CTT:['C']",
-    "17:41267741:ACTT:['AC']",
-    # "17:41267740:TACTT:['A']",
-    "17:41267795:GAC:['GA']",
-    "17:41267796:ACT:['A']",
-    "17:41267795:GA:['G']",
-    "17:41267742:CTTGCAAAATATGTGGTCACACTTTGTGGAGACAGGTTCCTTGATCAACTCCAGA:['C']"
-]
-
-insertion = [
-    "17:41276033:C:['CCAGATG']",
-    "17:41276032:AC:['ACCA']",
-    "17:41267795:G:['GAA']",
-    "17:41267797:CT:['CTAA']"
-]
-
-variants = [
-    *snps,
-    *deletions,
-    *insertion
-]
-
-
-
-def parse_vcf_id(vcf_id):
-    return vcf_id.replace("'", '').replace('[', '').replace(']', '').split(':')
-
-
-@pytest.fixture
-def vcf_path():
-    with tempfile.NamedTemporaryFile('w') as temp_vcf:
-        temp_vcf.write('##fileformat=VCFv4.0\n')
-        temp_vcf.write('##contig=<ID=17,length=81195210>\n')
-        temp_vcf.write('#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n')
-
-        for v in variants:
-            temp_vcf.write('%s\t%s\t1\t%s\t%s\t.\t.\t.\n'
-                           % tuple(parse_vcf_id(v)))
-
-        temp_vcf.flush()
-        yield temp_vcf.name
 
 
 def test_splicing_vcf_loads_all(vcf_path):
@@ -294,7 +242,7 @@ def test_splicing_vcf_loads_insertions(vcf_path):
         }
     ]
 
-    for i in range(len(insertion)):
+    for i in range(len(insertions)):
         d = next(dl)
         print(d)
         print(d['metadata']['ExonInterval']['start'])
