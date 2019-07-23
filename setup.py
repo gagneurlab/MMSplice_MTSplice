b05d13fefb02916bb41db5c7ec0d4779bd00f00b
Muhammed Hasan Celik <muhammedhasancelik@gmail.com>
2019-07-23 19:30:00 +0200

    fix for release config

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/gagneurlab/mmsplice',
-    version='1.0.0',
+    version='1.0.1',
     zip_safe=False
 )
