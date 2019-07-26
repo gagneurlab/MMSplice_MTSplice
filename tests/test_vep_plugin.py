9601aa01914d035e2f57cdcbd901ee6f2f3759de
Muhammed Hasan Celik <muhammedhasancelik@gmail.com>
2019-07-26 16:17:44 +0200

    vep plugin testcase fixes

--- tests/test_vep_plugin.py
+++ tests/test_vep_plugin.py
@@ -14,27 +14,12 @@ def test_vep_plugin():
     gtf = 'tests/data/test.gtf'
     vcf = 'tests/data/test.vcf.gz'
     fasta = 'tests/data/hg19.nochr.chr17.fa'
-    gtfIntervalTree = 'tests/data/test.pkl'  # pickle exon interval Tree
 
-    dl = SplicingVCFDataloader(gtfIntervalTree,
-                               fasta,
-                               vcf,
-                               out_file=gtfIntervalTree,
-                               split_seq=False, overhang=(100, 100))
+    dl = SplicingVCFDataloader(gtf, fasta, vcf)
+    model = MMSplice()
 
-    model = MMSplice(
-        exon_cut_l=0,
-        exon_cut_r=0,
-        acceptor_intron_cut=6,
-        donor_intron_cut=6,
-        acceptor_intron_len=50,
-        acceptor_exon_len=3,
-        donor_exon_len=5,
-        donor_intron_len=13)
-
-    df_python = predict_all_table(model, dl, batch_size=1024,
-                                  split_seq=False, assembly=True,
-                                  pathogenicity=True, splicing_efficiency=True)
+    df_python = predict_all_table(
+        model, dl, pathogenicity=True, splicing_efficiency=True)
     df_python_predictionsMax = max_varEff(df_python).set_index('ID')
 
     df_plugin = read_vep(vep_output)
@@ -44,8 +29,8 @@ def test_vep_plugin():
                    set(df_python_predictionsMax.index))
 
     vep_plugin_dlogitPsi = df_plugin_predictionsMax.loc[indexes,
-                                                        'mmsplice_dlogitPsi']
+                                                        'mmsplice_delta_logit_psi']
     python_package = df_python_predictionsMax.loc[indexes,
-                                                  'mmsplice_dlogitPsi']
+                                                  'delta_logit_psi']
 
     assert pearsonr(vep_plugin_dlogitPsi, python_package)[0] >= 0.99
