5a2a11fa189e8d94341d400ce44b8158ba0264a0
Muhammed Hasan Celik <muhammedhasancelik@gmail.com>
2019-11-04 19:13:28 +0100

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/gagneurlab/mmsplice',
-    version='1.0.1',
+    version='1.0.2',
     zip_safe=False
 )
