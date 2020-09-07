dd91265d4eafdadaa75990dd425af5b583aed101
Jun Cheng <chengju@in.tum.de>
2020-09-07 12:00:12 +0200

    Bump version: 2.1.0 → 2.1.1

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/gagneurlab/mmsplice',
-    version='2.1.0',
+    version='2.1.1',
     zip_safe=False
 )
