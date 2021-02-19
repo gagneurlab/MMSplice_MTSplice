60eee3b3803c110358aa6a2eb864a09be8742035
Jun Cheng <s6juncheng@gmail.com>
2021-02-19 11:41:15 +0100

    custom object

--- mmsplice/mtsplice.py
+++ mmsplice/mtsplice.py
@@ -57,8 +57,7 @@ class MTSplice:
             self.mtsplice_models = [load_model(
                 m, custom_objects=custom_objects) for m in MTSPLICE]
         else:
-            self.mtsplice_models = [load_model(
-                m, custom_objects=custom_objects) for m in MTSPLICE]
+            self.mtsplice_models = [load_model(m) for m in MTSPLICE]
         self.spliter = seq_spliter or SeqSpliter()
 
     def predict_on_batch(self, batch):
